58c86cf4ab772d634f7e19edd07f38e86fd51eb6
Mohamed Ashref <AshrefKing523@Gmail.com>
2026-03-19 11:50:07 +0200

    Add project files.

---
+++ PrintSpoolerCleaner/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+using System.Threading;
+
+namespace PrintSpoolerCleaner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string spoolPath = @"C:\Windows\System32\spool\PRINTERS";
+
+            try
+            {
+                ServiceController sc = new ServiceController("Spooler");
+
+                // إيقاف الخدمة
+                if (sc.Status != ServiceControllerStatus.Stopped)
+                {
+                    Console.WriteLine("Stopping Print Spooler...");
+                    sc.Stop();
+                    sc.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromSeconds(10));
+                }
+
+                // حذف الملفات
+                Console.WriteLine("Deleting spool files...");
+                if (Directory.Exists(spoolPath))
+                {
+                    DirectoryInfo di = new DirectoryInfo(spoolPath);
+
+                    foreach (FileInfo file in di.GetFiles())
+                    {
+                        try
+                        {
+                            file.Delete();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error deleting file {file.Name}: {ex.Message}");
+                        }
+                    }
+                }
+
+                // تشغيل الخدمة مرة تانية
+                Console.WriteLine("Starting Print Spooler...");
+                sc.Start();
+                sc.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromSeconds(10));
+
+                Console.WriteLine("Done.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
